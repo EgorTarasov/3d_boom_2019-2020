668c3bef40e6dc353337f06e35b1d8eb702cf249
Тарасов Егор <tarasov.egor.yandex@gmail.com>
2020-02-27 10:17:57 +0300

    First ballon on map

--- WebApp/bins/models.py
+++ WebApp/bins/models.py
@@ -6,6 +6,8 @@ class Bins (models.Model):
     field_addres = models.CharField(max_length=100, help_text='Адрес мусорки')
     field_data = models.IntegerField(default=0, help_text='Процент заполнености мусорки ')
     ip_addres = models.CharField(max_length=11, help_text='ip addres')
+    cordinates_x = models.FloatField(help_text='Координата мусорки', default=55.785078)
+    corfinates_y = models.FloatField(help_text='Координата мусорки', default=37.453163)
     def ref(self, data):
             self.field_data = data
             self.save()
