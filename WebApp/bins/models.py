b437ef43bea63ad2723472c95977451920ad8b5a
Тарасов Егор <tarasov.egor.yandex@gmail.com>
2020-02-27 11:54:05 +0300

    Auto generating bins on map

--- WebApp/bins/models.py
+++ WebApp/bins/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Bins (models.Model):
     field_addres = models.CharField(max_length=100, help_text='Адрес мусорки')
     field_data = models.IntegerField(default=0, help_text='Процент заполнености мусорки ')
+    field_waste = models.IntegerField(default=0, help_text='Второй датчик')
     ip_addres = models.CharField(max_length=11, help_text='ip addres')
     cordinates_x = models.FloatField(help_text='Координата мусорки', default=55.785078)
     corfinates_y = models.FloatField(help_text='Координата мусорки', default=37.453163)
